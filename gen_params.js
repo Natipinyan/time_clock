ff34bb41ffd478f9e0ef1f80d5039c7fbec3471b
nati-pinyan <natipinyan@gmail.com>
2023-09-23 17:56:32 +0300

    Initial commit

---
+++ gen_params.js
@@ -0,0 +1,4 @@
+exports.HOST ="localhost";
+exports.USER ="root";
+exports.PASSWORD ="";
+exports.DATABASE ="time_clock";
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const port = 4040;
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "js")));
+
+var db_M = require('./database');
+global.db_pool = db_M.pool;
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
+});
+
+
